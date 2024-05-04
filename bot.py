7eef832952914847ee7900efc9d2556d5fea6b65
H4ppyN4p <tobi5703@stud.kea.dk>
2024-05-04 17:47:30 +0200

    Bot sends message on channel creation

--- bot.py
+++ bot.py
@@ -10,12 +10,11 @@ intents = Intents.default()
 intents.message_content = True
 intents.members = True
 
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+#Startup call
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to discord successfully')
@@ -28,31 +27,38 @@ async def test_alive(ctx):
     await ctx.send('Its testing at least')
 
 @bot.command(name='submit')
-
 @commands.check(is_chnanel)
 async def create_channel(ctx):
-    new_channel_name = str(ctx.message.author)
+
+    #Variables
     guild = ctx.guild
     channels = guild.channels
-    does_not_exist = True
-    category = get(ctx.guild.categories, name='|—————APPLICATION—————|')
-    for channel in channels:
-        if channel.name == new_channel_name:
-            print('channel already exists')
-            does_not_exist = False
 
-    
+    does_not_exist = True
+
+    new_channel_name = str(ctx.message.author)
+    category = get(ctx.guild.categories, name='|—————APPLICATION—————|')
 
     overwrites = {
         guild.default_role : PermissionOverwrite(read_messages=False),
         get(ctx.guild.roles, name='Reviewer'): PermissionOverwrite(read_messages=True),
         ctx.message.author: PermissionOverwrite(read_messages=True)
-    }        
+    } 
+
+    for channel in channels:
+        if channel.name == new_channel_name:
+            print('channel already exists')
+            does_not_exist = False        
 
     if does_not_exist == True:
         print(ctx.message.author)
         await guild.create_text_channel(new_channel_name, category= category, overwrites=overwrites)      
         print('created new channel with name of: ' + str(ctx.message.author))  
+        created_channel = get(ctx.guild.channels, name=new_channel_name)   
+        await created_channel.send('hello there')
+
+
+
 
 @create_channel.error
 async def create_channel_error(ctx, error):
